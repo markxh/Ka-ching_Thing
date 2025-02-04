a43bc4002911a6a7613e5b99b5413d319fd27cf4
markxh <markharper.sa@gmail.com>
2025-02-04 08:07:39 +0200

    [feature] Updated to use BigDecimal instead of Double when working with money

--- app/src/main/java/co/za/markxh/ka_chingthing/feature/landing/presentation/LandingState.kt
+++ app/src/main/java/co/za/markxh/ka_chingthing/feature/landing/presentation/LandingState.kt
@@ -1,7 +1,9 @@
 package co.za.markxh.ka_chingthing.feature.landing.presentation
 
+import java.math.BigDecimal
+
 data class LandingState(
-    var inputAmount: Double = 0.00,
-    var totalAmount: Double = 0.00,
-    var itemAmounts: ArrayList<Double> = arrayListOf()
+    var inputAmount: BigDecimal = BigDecimal.ZERO,
+    var totalAmount: BigDecimal = BigDecimal.ZERO,
+    var itemAmounts: ArrayList<BigDecimal> = arrayListOf()
 )
