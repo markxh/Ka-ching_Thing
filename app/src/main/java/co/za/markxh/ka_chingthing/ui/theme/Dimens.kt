9a5b779d839661fda196307a57d2aa05ebd931f3
markxh <markharper.sa@gmail.com>
2025-04-06 21:34:13 +0200

    [feature] Add dimens and strings

---
+++ app/src/main/java/co/za/markxh/ka_chingthing/ui/theme/Dimens.kt
@@ -0,0 +1,19 @@
+package co.za.markxh.ka_chingthing.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+// TODO cleanup duplicates and naming conventions
+object Dimens {
+    val TopSectionVerticalPadding = 64.dp
+    val TopSectionBottomPadding = 34.dp
+    val InputAmountFontSize = 38.sp
+    val KeyPadButtonHeight = 90.dp
+    val BottomSectionVerticalPadding = 64.dp
+    val ItemAmountBottomPadding = 12.dp
+    val TotalAmountTopPadding = 12.dp
+    val BottomSectionHorizontalPadding = 16.dp
+    val ItemAmountEndPadding = 100.dp
+    val KeyPadButtonBorderWidth = 1.dp
+    val ItemAmountFontSize = 20.sp
+}
